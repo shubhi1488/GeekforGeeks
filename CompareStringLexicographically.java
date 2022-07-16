44b11daa900b7307ff5f4d8cc2abe271b3b987ac
Shubhi Agarwal <shubhi1488@gmail.com>
2022-07-16 13:51:10 +0530

    Create CompareStringLexicographically.java

---
+++ CompareStringLexicographically.java
@@ -0,0 +1,46 @@
+/*
+program to compare two strings lexicographically without using library function compareTo-------------------------------------------------------
+*/
+
+public class CompareStrings {
+	public static int compare(String str1, String str2){
+		for(int i=0;i<str1.length()&&i<str2.length();++i) {
+			if((int)str1.charAt(i)==(int)str2.charAt(i)) {
+				continue;
+			}
+			else {
+				return (int)str1.charAt(i)-(int)str2.charAt(i);
+			}
+		}
+		if(str1.length()<str2.length()) {
+			return (str1.length()-str2.length());
+		}
+		else if(str1.length()> str2.length()) {
+			return (str1.length()-str2.length());
+		}
+		else {
+			return 0;
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		 String string1 = new String("Geeks");
+	        String string2 = new String("Practice");
+	        String string3 = new String("Geeks");
+	        String string4 = new String("Geeksforgeeks");
+	     
+	        System.out.println(compare(string1,string2));
+	                                       
+	        System.out.println(compare(string1,string3));
+	        System.out.println(compare(string2,string1));
+	 
+	        // To show for edge case
+	        // In these cases, the output is the difference of
+	        // length of the string
+	        System.out.println(compare(string1,string4));
+	        System.out.println(compare(string4,string1));
+
+	}
+
+}

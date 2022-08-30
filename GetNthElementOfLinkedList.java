e859e754cdaecd0d180335d1c3a507eed0874a76
Shubhi Agarwal <shubhi1488@gmail.com>
2022-08-30 22:52:49 +0530

    Update GetNthElementOfLinkedList.java

--- GetNthElementOfLinkedList.java
+++ GetNthElementOfLinkedList.java
@@ -43,3 +43,53 @@ Write a function to get Nth node in a Linked List
         System.out.println("element found at index:"+obj.getNth(2));
     }
 }
+..............................................................................................................................................................................
+  #recursive approach-
+  public class Main
+{
+    Node head;
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+     static Node push(Node head, int new_data)
+    {
+        /* allocate node */
+        Node new_node = new Node(new_data);
+ 
+        new_node.next = head;
+ 
+        head = new_node;
+ 
+        return head;
+    }
+ 
+
+    static int getnth(Node head,int n){
+        int count=0;
+        if(head==null) return -1;
+        if(count==n)
+            return head.data;
+        return getnth(head.next,n-1);    
+    }
+    public static void main(String args[]){
+         Node head = null;
+ 
+        /* Use push() to construct below list
+        1.12.1.4.1 */
+        head = push(head, 1);
+        head = push(head, 4);
+        head = push(head, 1);
+        head = push(head, 12);
+        head = push(head, 1);
+ 
+        /* Check the count function */
+        System.out.printf("Element at index 3 is %d",
+                          getnth(head, 3));
+    }
+    
+}
